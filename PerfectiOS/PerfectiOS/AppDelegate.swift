188720995046a7ac691c5adc5d6b6724df3488dd
Kirill Khudyakov <kvkhudy1@mts.ru>
2019-04-30 16:16:41 +0300

    Init Xcode project

---
+++ PerfectiOS/PerfectiOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  PerfectiOS
+//
+//  Created by Кирилл Худяков on 30/04/2019.
+//  Copyright © 2019 Fun. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+ 
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+
+}
+
---
+++ PerfectiOS/PerfectiOS/Screens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerfectiOS
+//
+//  Created by Кирилл Худяков on 30/04/2019.
+//  Copyright © 2019 Fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .green
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
