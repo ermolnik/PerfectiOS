155f115081a8af245e3df2652c71c4733256029f
Kirill Khudyakov <hudiakov@gmail.com>
2019-05-01 20:10:27 +0300

    Added description to Person

--- PerfectiOS/PerfectiOS/Models/Person.swift
+++ PerfectiOS/PerfectiOS/Models/Person.swift
@@ -14,3 +14,25 @@ class Person {
     var photo: Data?
     var pictureUrl: String?
 }
+
+extension Person: CustomStringConvertible {
+    var description: String {
+        return "\nPerson: \(name ?? "none") \(pictureUrl ?? "none")"
+    }
+}
+
+extension Person {
+    func from(item: [String: AnyObject]) {
+        print(item)
+        if
+            let firstName = item["name"]?["first"] as? String,
+            let lastName = item["name"]?["last"] as? String
+        {
+            self.name = "\(firstName) \(lastName)"
+        }
+        
+        if let girlPictureUrl = item["picture"]?["large"] as? String {
+            self.pictureUrl = girlPictureUrl
+        }
+    }
+}
