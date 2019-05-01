f7e18208f15d2316a0979b0d031e19c0695d7efc
Kirill Khudyakov <hudiakov@gmail.com>
2019-05-01 13:22:24 +0300

    Added Person

---
+++ PerfectiOS/PerfectiOS/Models/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  PerfectiOS
+//
+//  Created by Kirill Khudiakov on 01/05/2019.
+//  Copyright © 2019 Fun. All rights reserved.
+//
+
+import Foundation
+
+
+class Person {
+    var name: String?
+    var photo: Data?
+    var pictureUrl: String?
+}
